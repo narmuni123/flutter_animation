911fd538a3f2da3afddd849b70d25f33d828a4a4
narmuni123 <narmuni123@gmail.com>
2023-05-16 12:24:15 +0530

    Added hero aniimation folder

---
+++ lib/animations/horo_animations/hero_animation_displace.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Displace extends StatefulWidget {
+  const Displace({Key? key}) : super(key: key);
+
+  @override
+  State<Displace> createState() => _DisplaceState();
+}
+
+class _DisplaceState extends State<Displace> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/data/widget_list.dart
+++ lib/data/widget_list.dart
@@ -1,10 +1,12 @@
 import 'package:animation/animations/chained_animation.dart';
+import 'package:animation/animations/horo_animations/hero_animation_displace.dart';
 import 'package:animation/animations/square_rotating.dart';
 import 'package:animation/animations/stack_and_rotate_3D.dart';
 import 'package:flutter/cupertino.dart';
 
 Map<String, Widget> animationWidgets = {
-  "Square Rotation Z" : const SquareRotating(),
-  "Chained Animation" : const ChainedAnimation(),
-  "Stack And Rotate 3D" : const StackAndRotate3D(),
+  "Square Rotation Z": const SquareRotating(),
+  "Chained Animation": const ChainedAnimation(),
+  "Stack And Rotate 3D": const StackAndRotate3D(),
+  "Hero Animation Displace": const Displace(),
 };

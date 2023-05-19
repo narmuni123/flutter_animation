a378574ae1243972bc5b28be1825132351c99eed
narmuni123 <narmuni123@gmail.com>
2023-05-19 12:26:25 +0530

    Added a list view

--- lib/animations/horo_animations/hero_animation_displace.dart
+++ lib/animations/horo_animations/hero_animation_displace.dart
@@ -1,5 +1,5 @@
+import 'package:animation/data/widget_list.dart';
 import 'package:flutter/material.dart';
-
 
 class Displace extends StatefulWidget {
   const Displace({Key? key}) : super(key: key);
@@ -9,8 +9,30 @@ class Displace extends StatefulWidget {
 }
 
 class _DisplaceState extends State<Displace> {
+  const List<Person> person = [
+    Person(name: "John", age: 20, emoji: "😃"),
+    Person(name: "Jane", age: 22, emoji: "😁"),
+    Person(name: "Jack", age: 23, emoji: "😂"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("People"),
+      ),
+      body: ListView.builder(
+        itemCount: person.length,
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemBuilder: (_, int index) {
+          return ListTile(
+            leading: Text(person[index].emoji),
+            title: Text(person[index].name),
+            subtitle: Text("${person[index].age} years"),
+          );
+        },
+      ),
+    );
   }
 }

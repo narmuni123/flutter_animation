2012da39884c0786fe20c87d3c680a336be03a8f
narmuni123 <narmuni123@gmail.com>
2023-05-30 11:37:13 +0530

    Added rotation animation

--- lib/animations/custom_paint/custom_shapes_animation.dart
+++ lib/animations/custom_paint/custom_shapes_animation.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:animation/data/clipper_constant.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,12 @@ class _CustomShapesAnimationState extends State<CustomShapesAnimation>
   late AnimationController _sidesController;
   late Animation<int> _sidesAnimation;
 
+  late AnimationController _radiusController;
+  late Animation<double> _radiusAnimation;
+
+  late AnimationController _rotationController;
+  late Animation<double> _rotationAnimation;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -25,6 +33,40 @@ class _CustomShapesAnimationState extends State<CustomShapesAnimation>
       begin: 3,
       end: 10,
     ).animate(_sidesController);
+
+    _radiusController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+
+    _radiusAnimation = Tween(
+      begin: 20.0,
+      end: 400.0,
+    ).chain(CurveTween(curve: Curves.bounceInOut)).animate(_radiusController);
+
+    _rotationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+
+    _rotationAnimation = Tween(
+      begin: 0.0,
+      end: 2.0 * pi,
+    ).chain(CurveTween(curve: Curves.easeInOut)).animate(_rotationController);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _sidesController.dispose();
+    _radiusController.dispose();
+    _rotationController.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    _sidesController.repeat(reverse: true);
+    _radiusController.repeat(reverse: true);
+    _rotationController.repeat(reverse: true);
   }
 
   @override
@@ -34,11 +76,29 @@ class _CustomShapesAnimationState extends State<CustomShapesAnimation>
       appBar: AppBar(
         backgroundColor: Colors.white12,
       ),
-      body: CustomPaint(
-        painter: Polygon(sides: 3),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width,
+      body: Center(
+        child: AnimatedBuilder(
+          animation: Listenable.merge([
+            _sidesController,
+            _radiusController,
+            _rotationController,
+          ]),
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..rotateX(_rotationAnimation.value)
+                ..rotateY(_rotationAnimation.value)
+                ..rotateZ(_rotationAnimation.value),
+              child: CustomPaint(
+                painter: Polygon(sides: _sidesAnimation.value),
+                child: SizedBox(
+                  width: _radiusAnimation.value,
+                  height: _radiusAnimation.value,
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
